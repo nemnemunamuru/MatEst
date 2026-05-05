13946ad0ca6afed1dcffe448a0fcd785b2906198
nemunemunamuru <murakami.naruse@jp.panasonic.com>
2026-05-05 16:16:49 +0900

    Remove MatEst_clean from repo and ignore it

--- MatEst_clean/matest/__init__.py
+++
@@ -1,6 +0,0 @@
-"""MatEst package: public API"""
-from .io import read_table_with_type
-from .estimator import estimate
-from .plotting import make_four_plots
-
-__all__ = ["read_table_with_type", "estimate", "make_four_plots"]
--- MatEst_clean/run_ftheta_gui.py
+++
@@ -1,4 +0,0 @@
-if __name__ == '__main__':
-    # defer importing the GUI class until runtime to reduce module import time
-    from matest.gui import App
-    App().mainloop()
